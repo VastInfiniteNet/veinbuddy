c48ac1c31b6880ad428fa57e7239db3edda1e5d9
VastInfiniteNet <screwthisusernameprocess@gmail.com>
2025-04-30 23:27:27 -0400

    Orinnari 1.21.4 changes

--- src/main/java/com/github/sbobicus/mixins/ParticleMixin.java
+++ src/main/java/com/github/sbobicus/mixins/ParticleMixin.java
@@ -2,17 +2,29 @@ package com.github.sbobicus.mixins;
 
 import net.minecraft.client.particle.ItemPickupParticle;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemPickupParticle.class)
-public class ParticleMixin {
-  @Inject(method = "buildGeometry", at = @At("HEAD"), cancellable = true)
-  public void particles(VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks, CallbackInfo ci) {
-    ci.cancel();
-  }
+public abstract class ParticleMixin {
+    @Inject(
+        method = "renderCustom",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void veinbuddy$preventItemCustomRender(
+        final @NotNull MatrixStack matrices,
+        final @NotNull VertexConsumerProvider vertexConsumers,
+        final @NotNull Camera camera,
+        final float tickDelta,
+        final @NotNull CallbackInfo ci
+    ) {
+        ci.cancel();
+    }
 }
